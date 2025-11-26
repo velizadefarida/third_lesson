e5d54ba45ae94588b342f47890d4e56981dcb0f8
Farida <velizadefarida@gmail.com>
2025-11-26 14:57:10 +0500

    8 commit

--- main.cpp
+++ main.cpp
@@ -1,28 +1,45 @@
 #include <iostream>
 #include "planar.hpp"
-#include "poin.hpp"
+#include "point.hpp"
 #include "function.hpp"
 #include "vector.hpp"
 
 int main()
 {
-  Planar* pls[10] = {};
-  size_t k = 0;
-  for (size_t i = 0; i < 10; ++i)
+  size_t s = 0;
+  size_t c = 20;
+  Planar** pls = new Planar*[c];
+  while (std::cin)
   {
+    Planar* pl = nullptr;
     try
     {
-      pls[k] = make(i % 2);
+      pl = make(std::cin);
+      if (s == c)
+      {
+        Planar** epls = new Planar*[c*2];
+        for (size_t i = 0; i < s; ++i)
+        {
+          epls[i] = pls[i];
+        }
+        delete[] pls;
+        pls = epls;
+        c *= 2;
+      }
+      pls[s++] = pl;
     }
-    catch (...)
+    catch(...)
     {
-      free_planars(pls, k);
+      delete pl;
+      free_planars(pls, s);
+      delete[] pls;
       return 2;
     }
-    ++k;
   }
-  draw(mostleft(pls, k));
-  free_planars(pls, k);
-  return 0;
+  if (!std::cin.eof())
+  {
+    free_planars(pls, s);
+    delete[] pls;
+    return 3;
+  }
 }
-
